8ada711b89db4e48ee85ddfb9a237091291a108a
fuwenke111 <3577539873@qq.com>
2021-12-05 00:24:56 +0800

    在Person类中增加了一个get和set方法

--- TestModule01/src/com/fwk/Person.java
+++ TestModule01/src/com/fwk/Person.java
@@ -2,4 +2,12 @@ package com.fwk;
 
 public class Person {
     private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
